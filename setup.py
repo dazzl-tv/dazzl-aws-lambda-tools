90b673af674c61f121fe550ae2850e11cf0d7ec7
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2018-09-18 16:57:23 +0200

    Add configuration for package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dazzl',
+    version='0.1',
+    author='VAILLANT Jeremy',
+    author_email='jeremy@dazzl.tv',
+    description='Library python for simplify to create lambda function (AWS lambda) and Dazzl API service.',
+    long_description=long_description,
+    long_desciption_content_type="text/markdown",
+    url='http://github.com/dazzl-tv/dazzl-lambda',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'os',
+        'logging',
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)

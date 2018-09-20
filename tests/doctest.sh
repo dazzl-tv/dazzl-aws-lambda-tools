bb1af5d5440d5b094a7fb3d9d3a082ac03a976e0
Vaillant Jeremy <jeremy.vaillant@dazzl.tv>
2018-09-20 15:17:09 +0200

    Fix exit status when test failed

--- tests/doctest.sh
+++ tests/doctest.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-m=None,
-name=None,
-globs=None,
-verbose=None,
-report=True,
-optionflags=0,
-extraglobs=None,
-raise_on_error=False,
-exclude_empty=False
+files=("bucket.py" "environment.py" "headers.py" "logger.py" "parameters.py")
+codes=()
+
+function run_test {
+  echo "Number of file tested : ${#files[*]}\r\n"
+  for ix in ${!files[*]}
+  do
+    test_file ${files[$ix]} $ix
+  done
+}
 
 function test_file {
   echo "## Test $1 class"
   python3 $1
-  echo ''
+  codes[$2]=$?
+  echo
 }
 
-cd dazzl_aws_lambda_tools
+function exit_with_status {
+  for ix in ${!files[*]}
+  do
+    if [ ${codes[$ix]} == 1 ]; then
+      echo "Error in test for file ${files[$ix]}"
+      exit ${codes[$ix]}
+    fi
+  done
+}
 
-test_file bucket.py
-test_file environment.py
-test_file headers.py
-test_file logger.py
-test_file parameters.py
+echo 'Get lib folder ...'
+cd dazzl_aws_lambda_tools
+run_test
+exit_with_status
